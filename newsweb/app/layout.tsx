0658b55afbc3111f80dc7c467c54ed43dbd62684
kumar11jr <kumar11jr@gmail.com>
2023-08-20 00:41:32 +0530

    last commit

--- newsweb/app/layout.tsx
+++ newsweb/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header/>
-        <div>{children}</div>
+        <div className='max-w-6xl mx-auto'>{children}</div>
         </body>
     </html>
   )
